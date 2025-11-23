1bca5a6445f28e9210ae140c03b0d1e2488cee05
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-23 16:47:56 +0600

    project setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// import Navbar from "./components/Navbar";
+import Navbar from "@/app/components/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,10 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* navbar */}
+        <Navbar></Navbar>
+
         {children}
       </body>
     </html>
